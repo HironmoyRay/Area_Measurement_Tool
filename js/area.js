b7fca7c04c8646cdea48eaf6ddc366d16fa8cd9c
hironmoy-ray <hpray35@gmail.com>
2024-03-07 01:50:48 +0600

    List of Area Calculation Done

--- js/area.js
+++ js/area.js
@@ -3,11 +3,15 @@ document.getElementById("triangle-btn").addEventListener("click", function () {
   const base = getInputFieldValue("triangle-base");
   const height = getInputFieldValue("triangle-height");
   const area = 0.5 * base * height;
-  if(isNaN(area)){
+  if (isNaN(area)) {
     return alert("Please Enter Valid Input");
   }
   const areaFixed = area.toFixed(2);
   setAreaValue("triangle-area", areaFixed);
+
+  const title = getTitle("triangle-title");
+//   console.log(title);
+  setReference(areaFixed, title);
 });
 
 // Area of Rectangle
@@ -17,11 +21,15 @@ document
     const width = getInputFieldValue("rectangle-width");
     const length = getInputFieldValue("rectangle-length");
     const area = width * length;
-    if(isNaN(area)){
-        return alert("Please Enter Valid Input");
-      }
+    if (isNaN(area)) {
+      return alert("Please Enter Valid Input");
+    }
     const areaFixed = area.toFixed(2);
     setAreaValue("rectangle-area", areaFixed);
+
+    const title = getTitle("rectangle-title");
+    // console.log(title);
+    setReference(areaFixed, title);
   });
 
 // Area of Parallelogram
@@ -31,50 +39,60 @@ document
     const base = getInputFieldValue("parallelogram-base");
     const height = getInputFieldValue("parallelogram-height");
     const area = base * height;
-    if(isNaN(area)){
-        return alert("Please Enter Valid Input");
-      }
+    if (isNaN(area)) {
+      return alert("Please Enter Valid Input");
+    }
     const areaFixed = area.toFixed(2);
     setAreaValue("parallelogram-area", areaFixed);
+
+    const title = getTitle("parallelogram-title");
+    // console.log(title);
+    setReference(areaFixed, title);
   });
 
 // Area of Rhombus
-document
-  .getElementById("rhombus-btn")
-  .addEventListener("click", function () {
-    const diagonal1 = getInputFieldValue("rhombus-diagonal1");
-    const diagonal2 = getInputFieldValue("rhombus-diagonal2");
-    const area = .5*diagonal1*diagonal2;
-    if(isNaN(area)){
-        return alert("Please Enter Valid Input");
-      }
-    const areaFixed = area.toFixed(2);
-    setAreaValue("rhombus-area", areaFixed);
-  });
+document.getElementById("rhombus-btn").addEventListener("click", function () {
+  const diagonal1 = getInputFieldValue("rhombus-diagonal1");
+  const diagonal2 = getInputFieldValue("rhombus-diagonal2");
+  const area = 0.5 * diagonal1 * diagonal2;
+  if (isNaN(area)) {
+    return alert("Please Enter Valid Input");
+  }
+  const areaFixed = area.toFixed(2);
+  setAreaValue("rhombus-area", areaFixed);
+
+  const title = getTitle("rhombus-title");
+//   console.log(title);
+  setReference(areaFixed, title);
+});
 
 //   Area of Pentagon
-document
-  .getElementById("pentagon-btn")
-  .addEventListener("click", function () {
-    const perimeter = getInputFieldValue("pentagon-perimeter");
-    const apothem = getInputFieldValue("pentagon-apothem");
-    const area = .5*perimeter*apothem;
-    if(isNaN(area)){
-        return alert("Please Enter Valid Input");
-      }
-    const areaFixed = area.toFixed(2);
-    setAreaValue("pentagon-area", areaFixed);
-  });
+document.getElementById("pentagon-btn").addEventListener("click", function () {
+  const perimeter = getInputFieldValue("pentagon-perimeter");
+  const apothem = getInputFieldValue("pentagon-apothem");
+  const area = 0.5 * perimeter * apothem;
+  if (isNaN(area)) {
+    return alert("Please Enter Valid Input");
+  }
+  const areaFixed = area.toFixed(2);
+  setAreaValue("pentagon-area", areaFixed);
+
+  const title = getTitle("pentagon-title");
+//   console.log(title);
+  setReference(areaFixed, title);
+});
 //   Area of Ellipse
-document
-  .getElementById("ellipse-btn")
-  .addEventListener("click", function () {
-    const radius1 = getInputFieldValue("radius1");
-    const radius2 = getInputFieldValue("radius2");
-    const area = Math.PI*radius1*radius2;
-    if(isNaN(area)){
-        return alert("Please Enter Valid Input");
-      }
-    const areaFixed = area.toFixed(2);
-    setAreaValue("ellipse-area", areaFixed);
-  });
+document.getElementById("ellipse-btn").addEventListener("click", function () {
+  const radius1 = getInputFieldValue("radius1");
+  const radius2 = getInputFieldValue("radius2");
+  const area = Math.PI * radius1 * radius2;
+  if (isNaN(area)) {
+    return alert("Please Enter Valid Input");
+  }
+  const areaFixed = area.toFixed(2);
+  setAreaValue("ellipse-area", areaFixed);
+
+  const title = getTitle("ellipse-title");
+//   console.log(title);
+  setReference(areaFixed, title);
+});
--- js/utility.js
+++ js/utility.js
@@ -10,3 +10,23 @@ function setAreaValue(areaFieldId,areaValue){
     const areaField = document.getElementById(areaFieldId);
     areaField.innerText = areaValue;
 }
+
+function getTitle(titleId){
+    const titleField = document.getElementById(titleId);
+    const title = titleField.innerText;
+    return title;
+
+}
+
+function setReference(area,title){
+const referenceField = document.getElementById("reference-field");
+const countChild = referenceField.childElementCount;
+// console.log(countChild);
+const p = document.createElement("p");
+p.innerHTML = `<span class="font-bold">${countChild+1}.</span>  Area of ${title} is <span class="font-bold">${area}</span>  cm <sup>2</sup>`;
+p.className="m-1 px-1 bg-sky-200 rounded-sm";
+
+referenceField.appendChild(p);
+
+
+}
